e900e93373215769cc12704a06e60c3c90769c79
UnicycleMatt <matt@unicyclelabs.com>
2015-03-16 22:52:57 -0700

    Completed strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,19 +12,31 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *favoriteString = @"My favorite cheese is";
+    NSString *cheeseString = [NSString stringWithFormat:@"%@ %@.", favoriteString, cheeseName];
+    
+    return cheeseString;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    if (cheeseRange.location != NSNotFound) {
+        NSString *cheeseNameWithoutSuffix = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+        return cheeseNameWithoutSuffix;
+    } else {
+        return cheeseName;
+    }
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        return @"1 cheese";
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSString *cheeseString = [NSString stringWithFormat:@"%ld cheeses", cheeseCount];
+        return cheeseString;
     }
     
     /*
