99d68355f231e69653d480f73bd8064c6ed75729
UnicycleMatt <matt@unicyclelabs.com>
2015-03-20 00:36:00 -0700

    Completed Swift exercise

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -7,8 +7,7 @@ Strings
 */
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
-    // WORK HERE
-    return cheese
+    return "My favorite cheese is \(cheese)"
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -21,13 +20,16 @@ Arrays & Dictionaries
 */
 
 let numberArray = [1, 2, 3, 4]
+
 // Add 5 to this array
-// WORK HERE
+var numberArrayMutable = numberArray + [5]
+
 
 let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
-// WORK HERE
-
+var numberDictionaryMutable = numberDictionary
+numberDictionaryMutable[5] = "five"
+numberDictionaryMutable
 /*
 
 Loops
@@ -35,10 +37,16 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+
+for number in 1...10 {
+    println("\(number)")
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+
+for number in 1..<11 {
+    println("\(number)")
+}
 
 let worf = [
     "name": "Worf",
@@ -57,8 +65,15 @@ let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    
+    var favoriteDrinks: [String] = []
+    
+    for character in characters {
+        if ((character["favorite drink"]) != nil) {
+            favoriteDrinks.append(character["favorite drink"]!)
+        }
+    }
+    return favoriteDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -77,6 +92,13 @@ let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
 
+func arrayToString(foodArray: Array<String>) -> String {
+    let stringRepresentation = ";".join(foodArray)
+    return stringRepresentation
+}
+
+arrayToString(strings)
+
 let expectedOutput = "milk;eggs;bread;challah"
 
 /*
@@ -89,3 +111,14 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+
+let cerealSortedAlphabetically = sorted(cerealArray, {(s1: String, s2: String) -> Bool in
+    return s1 < s2
+})
+
+
+
+
+
+
+
